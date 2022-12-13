34c06dae87a57ff19bd4167849e25dd1612efb80
Juan Garcia <juanlds621@gmail.com>
2022-12-13 15:51:07 +0000

    add gather

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -7,14 +7,12 @@ from sys import argv
 
 if __name__ == "__main__":
 
-    # First we connect/open/read/loads to the data url users
     url_first = get('https://jsonplaceholder.typicode.com/todos/')
     data_url_first = url_first.json()
     completed = 0
     TOTAL_NUMBER_OF_TASKS = 0
     NUMBER_OF_DONE_TASKS = []
 
-    # Second we connect/open/read/loads to the data url todos
     url_second = get('https://jsonplaceholder.typicode.com/users/')
     data_url_second = url_second.json()
 
@@ -30,7 +28,6 @@ if __name__ == "__main__":
         if i.get('id') == int(argv[1]):
             employee = i.get('name')
 
-    # Format the name of the completed tasks
     print("Employee {} is done with tasks({}/{}):".format(
         employee, completed, TOTAL_NUMBER_OF_TASKS))
 
