488fd2e711a339ad24063c2983ac561c1b39f7a0
Juan Garcia <juanlds621@gmail.com>
2022-12-13 03:19:05 +0000

    gather

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -2,29 +2,36 @@
 """script that, using this REST API, for a given employee ID, returns
 information about his/her TODO list progress."""
 import requests
-from sys import argv
+import sys
+
+
+def gather_data_from_api():
+    """
+        This function gather data from an api
+    """
+
+    url_todo = 'https://jsonplaceholder.typicode.com/todos?userId='
+    url_name = 'https://jsonplaceholder.typicode.com/users?id='
+    response_todo = requests.get(url_todo + sys.argv[1])
+    response_name = requests.get(url_name + sys.argv[1])
+
+    content_todo = list(response_todo.json())
+    content_name = list(response_name.json())
+
+    task_completed = 0
+    total_tasks = 0
+    completed_tasks = []
+    for elem in content_todo:
+        if elem['completed']:
+            task_completed += 1
+            completed_tasks.append(elem['title'])
+        total_tasks += 1
+
+    print("Employee {} is done with tasks({}/{}):".format(
+        content_name[0]['name'], task_completed, total_tasks))
+    for elem in completed_tasks:
+        print('\t ' + elem)
+
 
 if __name__ == "__main__":
-    if len(argv) > 1:
-        user = argv[1]
-        url = "https://jsonplaceholder.typicode.com/"
-        req = requests.get("{}users/{}".format(url, user))
-        name = req.json().get("name")
-        
-        if name is not None:
-            jreq = requests.get(
-                "{}todos?userId={}".format(url, user)).json()
-
-            alltsk = len(jreq)
-            completedtsk = []
-
-            for t in jreq:
-                if t.get("completed") is True:
-                    completedtsk.append(t)
-
-            count = len(completedtsk)
-            print("Employee {} is done with tasks({}/{}):"
-                  .format(name, count, alltsk))
-
-            for title in completedtsk:
-                print("\t {}".format(title.get("title")))
+    gather_data_from_api()
